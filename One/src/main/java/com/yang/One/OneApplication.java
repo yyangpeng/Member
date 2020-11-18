957ce29f7b0afbed6eb9739945da0a0d2ab1a7c6
yangyueyue <yangyueyueaa@163.com>
2020-11-19 00:51:41 +0800

    yangyueyue创建了项目One

---
+++ One/src/main/java/com/yang/One/OneApplication.java
@@ -0,0 +1,15 @@
+package com.yang.One;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yangyueyue
+ * @date 2020/11/19 - 0:48
+ */
+@SpringBootApplication
+public class OneApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OneApplication.class, args);
+    }
+}
---
+++ One/src/main/java/com/yang/entity/One.java
@@ -0,0 +1,10 @@
+package com.yang.entity;
+
+/**
+ * @author yangyueyue
+ * @date 2020/11/19 - 0:49
+ */
+public class One {
+    private String name;
+    private Integer age;
+}
