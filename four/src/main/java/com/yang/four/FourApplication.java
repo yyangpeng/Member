f642d51dcae71c0e80dc4e6e14dbcaa6fcc7c67a
jingyuyizhuiluo <1096715918@qq.com>
2020-11-19 02:02:29 +0800

    xiaoyao新建了分支Four

---
+++ four/src/main/java/com/yang/four/FourApplication.java
@@ -0,0 +1,11 @@
+package com.yang.four;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FourApplication.class, args);
+    }
+}
---
+++ four/src/main/java/com/yang/four/entity/Four.java
@@ -0,0 +1,6 @@
+package com.yang.four.entity;
+
+public class Four {
+    private String name;
+    private int age;
+}
